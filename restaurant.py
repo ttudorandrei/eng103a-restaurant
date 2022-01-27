574ed511ef9a3ffebd5a7dfbf9af38c62c93ea86
Tudor Tocan <tudor.tocan@gmail.com>
2022-01-27 17:12:06 +0000

    add order method

--- restaurant.py
+++ restaurant.py
@@ -1,3 +1,18 @@
 class Table:
-    def __init__(self):
+    def __init__(self, number_of_people):
         self.name = "Table"
+        self.number_of_people = number_of_people
+        self.bill = []
+
+    def order(self, item, price, quantity=1):
+        for i in self.bill:
+            if i and i["item"] == item:
+                i["quantity"] += quantity
+                print("first condition true")
+            elif i["item"] != item:
+                print("item not in list")
+                self.bill.insert({"item": item, "price": price, "quantity": quantity})
+
+        if len(self.bill) == 0:
+            self.bill.append({"item": item, "price": price, "quantity": quantity})
+
